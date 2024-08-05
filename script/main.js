d697138c422568d478e3f02b8b2c019acf400c0d
alexfiliuta <82522659+alexfiliuta@users.noreply.github.com>
2024-08-05 21:58:07 +0200

    Added JS functionality to the calculator

--- script/main.js
+++ script/main.js
@@ -1,1 +1,79 @@
-// functionality
+const display = document.getElementById("display");
+let first = "";
+let operator = "";
+let waiting = false;
+let errorState = false;
+
+function appendToDisplay(input){
+	if(errorState) {
+		display.value = input;
+		errorState = false;
+	} else if(waiting) {
+		display.value = input;
+		waiting = false;
+	} else {
+		display.value += input
+	}
+}
+
+function handleOperator(nextOperator){
+
+	if (errorState) {
+        clearDisp();
+        return;
+    }
+
+	if (operator && waiting) {
+        display.value = "Error";
+        first = "";
+        operator = "";
+        waiting = false;
+        errorState = true;
+        return;
+    }
+
+	if(first === ""){
+		first = display.value;
+	} else if(operator) {
+		const result = calculate(first, display.value, operator);
+		display.value = result;
+		first = result;
+	}
+
+	operator = nextOperator;
+	waiting = true;
+}
+
+function calculate(first, second, operator){
+	const firstNumber = parseFloat(first);
+	const secondNumber = parseFloat(second);
+
+	switch (operator) {
+	case `+`:
+		return firstNumber + secondNumber;
+	case `-`:
+		return firstNumber - secondNumber;
+	case `*`:
+		return firstNumber * secondNumber;
+	case `/`:
+		return firstNumber / secondNumber;
+	default:
+		return second;
+	}
+}
+
+function calculateResult(){
+	if(operator && first !== "") {
+		display.value = calculate(first, display.value, operator);
+		first = display.value;
+		operator = "";
+		waiting = false;
+	}
+}
+
+function clearDisp(){
+	display.value = "";
+	first = "";
+	operator = "";
+	waiting = false;
+}
